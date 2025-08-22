e8a1fca171eeed60be887b9a5e22baebb74c07c1
Om Gupta <ogupta4242@gmail.com>
2025-08-22 15:00:23 -0700

    feat(project): remove unnecessary items
    way too complex for this simple of a template

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/EmptyScreenContent.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/EmptyScreenContent.kt
@@ -5,16 +5,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kmp_app_template.composeapp.generated.resources.Res
-import kmp_app_template.composeapp.generated.resources.no_data_available
+import maleficmobile.composeapp.generated.resources.Res
+import maleficmobile.composeapp.generated.resources.no_data_available
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun EmptyScreenContent(
-    modifier: Modifier = Modifier,
-) {
+fun EmptyScreenContent(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center,
--- composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/MainViewController.kt
@@ -2,4 +2,5 @@ package com.jetbrains.kmpapp
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+@Suppress("ktlint:standard:function-naming", "unused", "FunctionName")
 fun MainViewController() = ComposeUIViewController { App() }
